559afdc5041a8e251a66bcf868cc537b9ddea96a
isntyet <isntyet@gmail.com>
2020-10-28 17:39:01 +0900

    련습용 세팅

--- src/main/java/com/isntyet/java/practice/stream/domain/Human.java
+++ src/main/java/com/isntyet/java/practice/stream/domain/Human.java
@@ -1,53 +1,33 @@
 package com.isntyet.java.practice.stream.domain;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
 
-@Data
-@Builder
-public class Human implements Comparable<Human> {
+@Entity
+@Getter
+@NoArgsConstructor
+public class Human {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long idx;
-    private String name;
-    private Integer money;
-    private LocalDate birth;
-    private List<String> travelDestinations;
 
-    public Human(Long idx, String name, Integer money, LocalDate birth, List<String> travelDestinations) {
+    private String name;
+
+    private Integer money;
+
+    private LocalDate birth;
+
+    public Human(String name, Integer money, LocalDate birth) {
         this.idx = idx;
         this.name = name;
         this.money = money;
         this.birth = birth;
-        this.travelDestinations = travelDestinations;
-    }
-
-    public boolean isNameStartwithZz() {
-        return this.name.startsWith("zz");
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Human human = (Human) o;
-        return Objects.equals(idx, human.idx) &&
-                Objects.equals(name, human.name) &&
-                Objects.equals(money, human.money) &&
-                Objects.equals(birth, human.birth) &&
-                Objects.equals(travelDestinations, human.travelDestinations);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idx, name, money, birth, travelDestinations);
-    }
-
-    @Override
-    public int compareTo(Human o) {
-        return this.name.compareTo(o.name);
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE human (
+    idx BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    money INT NOT NULL,
+    birth DATE
+);
